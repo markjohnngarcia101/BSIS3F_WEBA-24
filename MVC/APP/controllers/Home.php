7ec8da83c79816dbe241178bc88664a3fd602d1c
jsantiagoxiii <joanesantiagoxiii@gmail.com>
2024-02-23 16:49:20 +0800

    routing

---
+++ MVC/APP/controllers/Home.php
@@ -0,0 +1,27 @@
+<?php
+
+class Home
+{
+    public function index()
+    {
+        echo 'Home/Index';
+    }
+    public function test()
+    {
+        echo 'Test';
+
+    }
+
+    public function come()
+    {
+        echo 'If you want to view paradise
+        Simply look around and view it
+        Anything you want to, do it
+        Want to change the world?
+        Theres nothing to it
+        There is no life I know
+        To compare with pure imagination
+        Living there youll be free
+        If you truly wish to be ';
+    }
+}
