b23765824e1ea15b050dd537cf0bed371f27bed5
markjohnngarcia101 <markjohngarcia101@gmail.com>
2024-03-22 16:45:18 +0800

    MVC update

--- MVC/APP/CORE/init.php
+++ MVC/APP/CORE/init.php
@@ -1,5 +1,12 @@
 <?php
 
 require 'functions.php';
+require 'Database.php';
+require 'Model.php';
 require 'Controller.php';
 require 'App.php';
+
+spl_autoload_register(function ($clas_name){
+
+    require '../app/models/' .$clas_name . '.php';
+});
---
+++ MVC/MVC/APP/CORE/init.php
@@ -0,0 +1,5 @@
+<?php
+
+require 'functions.php';
+require 'Controller.php';
+require 'App.php';
